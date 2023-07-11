bc8c68954c42396b637ad632bd1fcb13fe582e98
Pinceur2Teton <138195484+Pinceur2Teton@users.noreply.github.com>
2023-07-11 15:37:37 +0200

    Update Fonction_capteurs.ino

--- Carte_recepteur_4/Fonction_capteurs.ino
+++ Carte_recepteur_4/Fonction_capteurs.ino
@@ -1,48 +1,35 @@
-// Fonction de lecture du capteur de température SI7021
-float readTemperature() {
-  Wire.beginTransmission(SI7021_ADDRESS);
-  Wire.write(SI7021_MEASURE_TEMPERATURE_HOLD);
-  Wire.endTransmission();
-  delay(500);
+void normalMeasurement() {
+  Wire.beginTransmission(SI7034_ADDRESS);                   // Début de la transmission à l'adresse du SI7034
+  Wire.write(hexCode >> 8);                                 // Ecriture de 0x78 puis ecriture de 0x66 pour faire la mesure en mode No Hold normal
+  Wire.write(hexCode);
+  Wire.endTransmission();                                   // Fin de la transmission une fois la donnée écrite
 
-  Wire.requestFrom(SI7021_ADDRESS, 3);
-  if (Wire.available() >= 3) {
-    byte msb = Wire.read();
-    byte lsb = Wire.read();
-    byte crc = Wire.read();
+  delay(15);                                                // Délais pour écrire dans le registre
 
-    int rawTemperature = (msb << 8) | lsb;
-    float temperature = ((175.72 * rawTemperature) / 65536.0) - 46.85 + 0.42;
+  uint8_t buf[6];                                           // Déclaration d'un tableaude 6 octet
+  Wire.requestFrom(SI7034_ADDRESS, 6);                      // Lecture à l'adresse du SI7034 de 6 octet                     
+  if (Wire.available() == 6) {                              // Tant que il y à 6 octet à lire
+    buf[0] = Wire.read();     // LSB                        // On remplis le tableau avec les octet recus
+    buf[1] = Wire.read();     // MSB
+    buf[2] = Wire.read();     // CRC
+    buf[3] = Wire.read();     // LSB
+    buf[4] = Wire.read();     // MSB
+    buf[5] = Wire.read();     // CRC
 
-    return temperature;
-  }
-  else {
-    Serial.println("\nErreur de lecture de la température");
-    return 0.0; // Valeur par défaut en cas d'erreur
+    temperature = readTemperature(buf[0] << 8 | buf[1]);    // Température prends la valeur de buf[0] décalé de 8 bits à gauche concaténé avec buff[1]
+    humidity = readHumidity(buf[3] << 8 | buf[4]);          // Humidité prends la valeur de buf[3] décalé de 8 bits à gauche concaténé avec buff[4]
+  } else {                                                  // Si il n'y avait pas de data de recus
+    // Erreur de lecture
+    temperature = 0;                                        // On renvoie 0
+    humidity = 0;
   }
 }
 
-// Fonction de lecture du capteur de humidité SI7021
-float readHumidity() {
-  Wire.beginTransmission(SI7021_ADDRESS);
-  Wire.write(SI7021_MEASURE_HUMIDITY_HOLD);
-  Wire.endTransmission();
-  delay(500);
+float readTemperature(uint16_t rawValue) {
+  return -45 + 175 * (rawValue / pow(2, 16));               // Calcul de la température avec la valeur raw obtenue
+}
 
-  Wire.requestFrom(SI7021_ADDRESS, 3);
-  if (Wire.available() >= 3) {
-    byte msb = Wire.read();
-    byte lsb = Wire.read();
-    byte crc = Wire.read();
-
-    int rawHumidity = (msb << 8) | lsb;
-    float humidity = ((125.0 * rawHumidity) / 65536.0) - 6.0 + 3.5;
-    humidity = humidity * 0.63;
-
-    return humidity;
-  }
-  else {
-    Serial.println("\nErreur de lecture de l'humidité");
-    return 0.0; // Valeur par défaut en cas d'erreur
-  }
+float readHumidity(uint16_t rawValue) {
+  return 100 * (rawValue / pow(2, 16));                     // Calcul de l'humidité avec la valeur raw obtenue
+}
 }
